2a62c884a9dc3d654b2319255134fac810a2ecb5
IrenyaFortin <irenya.fortin@gmail.com>
2024-11-06 18:41:18 -0600

    Subida inicial M1

---
+++ POOP1-T1M1-IRENYAFORTIN-202410060535/src/poop1/t1/irenyafortin/pkg202410060535/POOP1T1IRENYAFORTIN202410060535.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poop1.t1.irenyafortin.pkg202410060535;
+
+/**
+ *
+ * @author IRENYA FORTIN
+ */
+public class POOP1T1IRENYAFORTIN202410060535 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola, soy Irenya Annarella Fortin Burlero");
+    }
+    
+}
